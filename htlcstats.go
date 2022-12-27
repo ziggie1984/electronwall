0b6d534f2921955917124dc5c8bf2f969ab92347
ziggie <ziggie1984@protonmail.com>
2022-12-27 13:57:55 +0100

    add statistics

--- htlcstats.go
+++ htlcstats.go
@@ -50,30 +50,49 @@ func (stat *HtlcStatistics) count(event HtlcEvent) {
 }
 
 func NewHtlcStats(ctx context.Context) *HtlcStatistics {
-
 	return &HtlcStatistics{
 		LastUpdate: time.Now(),
 	}
 }
 
+func (stat *HtlcStatistics) get1Day() string {
+	return fmt.Sprintf("HTLC-Stats (1 Day): Settled %d, LinkFail: %d, ForwardFail %d, TotalForwards%d", stat.CounterWeek[stat.WeekDay].Settled,
+		stat.CounterWeek[stat.WeekDay].LinkFails, stat.CounterWeek[stat.WeekDay].ForwardFails, stat.CounterWeek[stat.WeekDay].ForwardEvents)
+}
+
+func (stat *HtlcStatistics) get7Days() string {
+	var totalWeek HtlcCounter
+	for _, dayStat := range stat.CounterWeek {
+		totalWeek.Settled += dayStat.Settled
+		totalWeek.ForwardEvents += dayStat.ForwardEvents
+		totalWeek.ForwardFails += dayStat.ForwardFails
+		totalWeek.LinkFails += dayStat.LinkFails
+	}
+	return fmt.Sprintf("HTLC-Stats (7 Days): Settled %d, LinkFail: %d, ForwardFail %d, TotalForwards%d", totalWeek.Settled,
+		totalWeek.LinkFails, totalWeek.ForwardFails, totalWeek.ForwardEvents)
+
+}
+
 func (stat *HtlcStatistics) postStats(ctx context.Context, telegramNotifier *TelegramNotifier) {
 
-	timer := time.NewTimer(time.Second * 60)
+	var cycleTime time.Duration = 60
+
+	timer := time.NewTimer(time.Second * cycleTime)
 	defer timer.Stop()
 	var wg sync.WaitGroup
 
 	for {
-		log.Debug("Posting HTLC Statistics")
 		wg.Add(1)
 		go func() {
 			<-timer.C
-			err := telegramNotifier.Notify(fmt.Sprintf("HTLC-Stats (1 Day): Settled %d, LinkFail: %d, ForwardFail %d, TotalForwards%d", stat.CounterWeek[stat.WeekDay].Settled,
-				stat.CounterWeek[stat.WeekDay].LinkFails, stat.CounterWeek[stat.WeekDay].ForwardFails, stat.CounterWeek[stat.WeekDay].ForwardEvents))
-
+			log.Debug("Posting HTLC Statistics")
+			err := telegramNotifier.Notify(stat.get1Day())
 			if err != nil {
 				log.Error("htlc stats error", err)
 			}
-			timer.Reset(time.Second * 60)
+			// Post 7 Days Stats
+			telegramNotifier.Notify(stat.get7Days())
+			timer.Reset(cycleTime)
 			wg.Done()
 		}()
 		wg.Wait()
@@ -85,7 +104,6 @@ func (stat *HtlcStatistics) DispatchHtlcStats(ctx context.Context, telegramNotif
 
 	// Htlc statistics
 	go func() {
-		log.Debug("PostStats")
 		stat.postStats(ctx, telegramNotifier)
 
 		// release wait group for channel acceptor
